257710a5e2c74fb15426a8d335ea3a5a8b6558dd
Mxcharia <138471248+Mxcharia@users.noreply.github.com>
2024-04-15 17:52:39 +0300

    Update wifi-connect.sh
     - fixes columns where the spaces inbetween wifi name

--- scripts/wifi-connect.sh
+++ scripts/wifi-connect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function wificonnect () {
-select wifi in $(nmcli device wifi list | sed -n '1!p' |cut -b 9-|awk '{print $2}') ;
+select wifi in $(nmcli device wifi list | sed -n '1!p' |cut -b 9-|awk -F" {2,}" '{print $2}') ;
 # read -s -p is not compatible with zsh
 do  read -s "password?Enter Your password: ";
 nmcli device wifi connect $wifi password $password;
