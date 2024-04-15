34974b7fa53b637b83498447481efd58554c3099
Stephen Wambugu <wambugumacharia35@gmail.com>
2024-04-15 18:03:27 +0300

    Update wifi-connect.sh
     - fixes breaking of lines by newlines instead of spaces

--- scripts/wifi-connect.sh
+++ scripts/wifi-connect.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 function wificonnect () {
+IFS=$'\n'
 select wifi in $(nmcli device wifi list | sed -n '1!p' |cut -b 9-|awk -F" {2,}" '{print $2}') ;
-# read -s -p is not compatible with zsh
+  # read -s -p is not compatible with zsh
 do  read -s "password?Enter Your password: ";
 nmcli device wifi connect $wifi password $password;
 break ; 
